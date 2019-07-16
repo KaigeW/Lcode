82b28849c92c68d85fe37e6ae56d1f8ae50ffa01
Kaige Wang <kaigew@uci.edu>
2019-07-16 15:06:28 -0700

    895 added

--- section90.java
+++ section90.java
@@ -1,6 +1,46 @@
 class Solution {
 
 
+   // Question 895
+   /**
+    * IDEA :
+    * Map freq is to map the frequency to each key,
+    * Map group is to map the stack of numbers to a frequency number
+    *
+    * Everytime we push, we need to update the frequency(+1), update the maxfreq
+    * if needed, push the number to its frequency stack(create one if no exist)
+    *
+    * Everytime we pop, we need to pop a number from the maxfreq stack, update
+    * the freq map, and also check if maxfreq stack is empty, if so, --maxfreq
+    */
+
+   Map<Integer, Integer> freq;
+   Map<Integer, Stack<Integer>> group;
+   int maxfreq;
+
+   public FreqStack() {
+       freq = new HashMap<>();
+       group = new HashMap<>();
+       maxfreq = 0;
+   }
+
+   public void push(int x) {
+       int f = freq.getOrDefault(x,0) + 1;
+       freq.put(x,f);
+       if( f > maxfreq)
+           maxfreq = f;
+
+       group.computeIfAbsent(f, z->new Stack()).push(x);
+   }
+
+   public int pop() {
+       int x = group.get(maxfreq).pop();
+       freq.put(x,freq.get(x) - 1);
+       if( group.get(maxfreq).size() == 0 )
+           --maxfreq;
+       return x;
+   }
+
    // Question 896
    /**
     * IDEA 1:
