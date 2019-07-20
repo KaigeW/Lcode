bd5bdb31db50353b85f2fa1da8bd511ae533da68
Kaige Wang <kaigew@uci.edu>
2019-07-19 18:32:32 -0700

    5, 10 needed

--- section1.java
+++ section1.java
@@ -371,4 +371,30 @@ class Solution {
             return true;
         }
 
+       // Question 10
+       /**
+        * IDEA 1:
+        * Recursive...
+        */
+       public boolean isMatch(String text, String pattern){
+           if( pattern.isEmpty())
+               return text.isEmpty();
+           boolean first_match = ( !text.isEmpty() &&
+                                    ( pattern.charAt(0) == text.charAt(0) ||
+                                        pattern.charAt(0) == '.'));
+           if( pattern.length() >= 2 && pattern.charAt(1) == '*'){
+               return (isMatch(text, pattern.substring(2)) ||
+                        (first_match && isMacthc(text.substring(1), pattern)));
+           }
+           else(
+               return first_match && isMatch(text.substring(1),
+                                             pattern.substring(1));
+           )
+       }
+
+       /**
+        * IDEA 2:
+        * Dynamic Programming
+        */
+
 }
