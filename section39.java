e47e863b90212e14e6160f6535cc6eb5337b5807
Kaige Wang <kaigew@uci.edu>
2019-09-08 15:12:49 -0700

    Two problems added

---
+++ section39.java
@@ -0,0 +1,45 @@
+class Solution {
+    // Question 394
+    /**
+     *  IDEA 1:
+     * Using Stack
+     **/
+    public String decodeString(String s) {
+        String res = "";
+        Stack<Integer> countStack = new Stack<>();
+        Stack<String> resStack = new Stack<>();
+        int idx = 0;
+        while (idx < s.length()) {
+            // count the number
+            if (Character.isDigit(s.charAt(idx))) {
+                int count = 0;
+                while (Character.isDigit(s.charAt(idx))) {
+                    count = 10 * count + (s.charAt(idx) - '0');
+                    idx++;
+                }
+                countStack.push(count);
+            }
+            // start a new String and push the old string into a Stack
+            if (s.charAt(idx) == '[') {
+                resStack.push(res);
+                res = "";
+                idx++;
+            }
+            // start appending the old String with the repeatedtimes new
+            // String
+            else if (s.charAt(idx) == ']') {
+                StringBuilder temp = new StringBuilder (resStack.pop());
+                int repeatTimes = countStack.pop();
+                for (int i = 0; i < repeatTimes; i++) {
+                    temp.append(res);
+                }
+                res = temp.toString();
+                idx++;
+            }
+            else {
+                res += s.charAt(idx++);
+            }
+        }
+        return res;
+    }
+}
