e53fce897a12636fb13c2614355d01cb49240577
Kaige <my476759488@hotmail.com>
2021-09-20 17:27:16 -0500

    Add one medium

--- section3.java
+++ section3.java
@@ -24,4 +24,80 @@ class Solution {
          }
          return ++start;
      }
+
+    // Question 29
+    /**
+     * IDEA 1:
+     * 剑指！自我实现
+     */
+     public int divide(int dividend, int divisor) {
+         boolean negative = false;
+         if( dividend == Integer.MIN_VALUE && divisor == -1 )
+             return Integer.MAX_VALUE;
+         if( dividend < 0 )
+             negative = negative ? false : true ;
+         else
+             dividend = -dividend;
+         if( divisor < 0 )
+             negative = negative ? false : true ;
+         else
+             divisor = -divisor;
+
+         int result = 0;
+         int multi = divisor;
+         int time = 1;
+         while( dividend <= divisor ) {
+             if( dividend <= multi ) {
+                 dividend -= multi;
+                 result += time;
+                 if( (long) time * 2 < Integer.MAX_VALUE ) {
+                     multi += multi;
+                     time += time;
+                 }
+             } else {
+                 multi /= 2;
+                 time /= 2;
+             }
+         }
+         if(negative) return -result;
+         return result;
+     }
+
+    /**
+     * IDEA 2:
+     * 剑指！官方实现
+     */
+     public int divide( int dividend, int divisor ) {
+         if ( dividend == 0x80000000 && divisor == -1 ) {
+             return Integer.MAX_VALUE;
+         }
+         int negative = 2;
+         if ( dividend > 0 ) {
+             negative--;
+             dividend = -dividend;
+         }
+         if ( divisor > 0 ) {
+             negative --;
+             divisor = - divisor;
+         }
+
+         int result = divideCore( dividend, divisor );
+         return negative == 1 ? -result : result;
+     }
+
+     private int divideCore( int dividend, int divisor ) {
+         int result = 0;
+         while ( dividend <= divisor ) {
+             int value = divisor;
+             int quotient = 1;
+             while ( value > 0xc0000000 && dividend <= value + value ) {
+                 quotient += quotient;
+                 value += value;
+             }
+             result += quotient;
+             dividend -= value;
+         }
+         return result;
+     }
+
 }
