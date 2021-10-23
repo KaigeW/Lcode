6f9e4e284d46215cb570c6546fe376e20935101f
Kaige <my476759488@hotmail.com>
2021-10-23 14:39:47 -0500

    one easy added

--- section12.java
+++ section12.java
@@ -1,4 +1,42 @@
 class Solution {
+    // question 112
+    /**
+     *  IDEA 1:
+     *  Traverse the tree, find out each possible path from root to leaf
+     *
+     **/
+     public boolean hasPathSum(TreeNode root, int targetSum) {
+         if( root != null )
+             return traverseTree( root, targetSum, root.val );
+         return false;
+     }
+
+     private boolean traverseTree( TreeNode node, int targetSum, int sum ) {
+         if( node == null )
+             return false;
+         // ATTENTION TO THE LAST CONDITION, it prechecks the equality
+         if( node.left == null && node.right == null && targetSum == sum + node.val )
+             return true;
+
+         return traverseTree( node.left, targetSum, sum + node.val)
+           || traverseTree( node.right, targetSum, sum + node.val);
+     }
+
+    /**
+     *  IDEA 2:
+     *  Traverse the tree, find out each possible path from root to leaf
+     *  Different tree traversal but still in in-order
+     **/
+
+     public boolean hasPathSum( TreeNode root, int targetSum ) {
+         if( root != null )
+             return false;
+         if( root.left == null && root.right == null && targetSum - root.val == 0)
+             return true;
+         return hasPathSum(root.left, targetSum - root.val)
+           || hasPathSum(root.right, targetSum - root.val);
+     }
+
     // question 118?
     /**
      *  IDEA 1:
