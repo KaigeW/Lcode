8c5a04ac93d2358455b304130eb4d561bf377ed9
Kaige <my476759488@hotmail.com>
2021-10-28 07:10:36 -0500

    solution added for one medium

--- section22.java
+++ section22.java
@@ -62,9 +62,34 @@ class Solution {
               return false;
           }
       }
-
      /**
       *  IDEA 2:
+      *
+      *  p.s. 剑指源码
+      **/
+      public boolean containsNearbyAlmostDuplicate( int[] nums, int k, int t) {
+          TreeSet<Long> set = new TreeSet<>();
+          for( int i = 0; i < nums.length; ++i ) {
+              Long lower = set.floor((long) nums[i]);
+              if( lower != null && lower >= (long) nums[i] - t ) {
+                  return true;
+              }
+
+              Long upper = set.ceiling( (long)nums[i] );
+              if( upper != null && upper <= (long) nums[i] + t ) {
+                  return true;
+              }
+
+              set.add( (long)nums[i] );
+              if( i >= k )
+                  set.remove((long)nums[i - k]);
+          }
+          return false;
+      }
+
+
+     /**
+      *  IDEA 3:
       *  todo
       **/
       public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
