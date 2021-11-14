7d119ab78e7b08ebd310fa1f82fa999f8edd6862
Kaige <my476759488@hotmail.com>
2021-11-14 15:22:32 -0600

    one medium copied

--- section68.java
+++ section68.java
@@ -1,4 +1,63 @@
 class Solution {
+    // Question 676
+    /**
+     * IDEA:
+     * Use prefix tree
+     *
+     * p.s. 剑指源码
+     *
+     */
+     class MagicDictionary {
+
+         static class TrieNode {
+             public TrieNode[] children;
+             public boolean isWord;
+
+             public TrieNode() {
+                 children = new TrieNode[26];
+             }
+         }
+
+         private TrieNode root;
+
+         public MagicDictionary() {
+             root = new TrieNode();
+         }
+
+         public void buildDict(String[] dictionary) {
+             for( String word: dictionary ) {
+                 TrieNode node = root;
+                 for( char ch: word.toCharArray() ) {
+                     if( node.children[ch-'a'] == null)
+                         node.children[ch-'a'] = new TrieNode();
+                     node = node.children[ch - 'a'];
+                 }
+                 node.isWord = true;
+             }
+         }
+
+         public boolean search(String searchWord) {
+             return dfs(root, searchWord, 0, 0);
+         }
+
+         private boolean dfs( TrieNode root, String word, int i , int edit ) {
+             if( root == null )
+                 return false;
+             if( root.isWord && i == word.length() && edit == 1 )
+                 return true;
+             if( i < word.length() && edit <= 1 ) {
+                 boolean found = false;
+                 for( int j = 0; j < 26 && !found; j++ ) {
+                     int next = j == word.charAt(i) - 'a' ? edit: edit + 1;
+                     found = dfs( root.children[j], word, i + 1, next);
+                 }
+                 return found;
+             }
+
+             return false;
+         }
+
+    }
 
     // Question 680
     /**
