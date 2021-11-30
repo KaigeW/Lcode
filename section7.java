9b575791b9078bf5756e12238b6a2fbd3a40ee56
Kaige <my476759488@hotmail.com>
2021-11-30 17:29:48 -0600

    one easy added

--- section7.java
+++ section7.java
@@ -56,4 +56,25 @@ class Solution {
          return result.reverse().toString();
      }
 
+
+    // Question 69
+    /**
+     * IDEA 1:
+     * Inspired by 剑指
+     */
+     public int mySqrt( int n ) {
+         int start = 1, end = n;
+         while( start <= end ) {
+             int mid = start + ( end - start ) / 2;
+             if( mid <= n / mid ) {
+                 if( (mid + 1) > n / (mid + 1) )
+                     return mid;
+                 start = mid + 1;
+             }
+             else
+                 end = mid - 1;
+         }
+         return 0;
+     }
+
 }
