d02dea4f31c838e9e536e0388aa5fc1f2c9d95e7
Kaige <my476759488@hotmail.com>
2021-12-20 06:43:47 -0600

    one hard copied(dynamic!!)

--- section14.java
+++ section14.java
@@ -41,6 +41,45 @@ class Solution {
 
 
 
+    // Question 132
+    /**
+     * IDEA 1:
+     * Dynamic programming
+     *
+     * p.s. 剑指源码
+     *
+     */
+
+     public int minCut( String s ) {
+         int len = s.length();
+         boolean[][] isPal = new boolean[len][len];
+
+         for( int i = 0; i < len; i++ ) {
+             for( int j = 0; j <= i; ++j ) {
+                 char ch1 = s.charAt(i);
+                 char ch2 = s.charAt(j);
+                 if( ch1 == ch2  && (i <= j + 1 || isPal[j + 1][i - 1]))
+                     isPal[j][i] = true;
+             }
+         }
+
+         int[] dp = new int[len];
+         for( int i = 0; i < len; ++i ) {
+             if( isPal[0][i] )
+                 dp[i] = 0;
+             else {
+                 dp[i] = i;
+                 for( int j = 1; j <= i; j++ ) {
+                     if( isPal[j][i]) {
+                         dp[i] = Math.min(dp[i], dp[j - 1] + 1);
+                     }
+                 }
+             }
+         }
+         return dp[len - 1];
+     }
+
+
     // Question 136
     /**
      * IDEA 1:
