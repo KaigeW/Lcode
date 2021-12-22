ab3cd95243b6ca1de32d1e2a344c5fd1383ab206
Kaige <my476759488@hotmail.com>
2021-12-21 19:18:08 -0600

    one medium added(dp)

--- section7.java
+++ section7.java
@@ -1,5 +1,74 @@
 class Solution {
 
+    // Question 62
+    /**
+     * IDEA 1:
+     * Dynamic Programming
+     *
+     * same as 剑指 idea2
+     *
+     */
+
+     public int uniquePaths(int m, int n) {
+         int[][] pathCount = new int[m][n];
+
+         for( int i = 0; i < m; ++i )
+             pathCount[i][0] = 1;
+
+         for( int i = 0; i < n; ++i )
+             pathCount[0][i] = 1;
+
+         for( int x = 1; x < m; ++x ) {
+             for( int y = 1; y < n; ++y ) {
+                 pathCount[x][y] = pathCount[x - 1][y] + pathCount[x][y - 1];
+             }
+         }
+
+         return pathCount[m - 1][n - 1];
+     }
+
+    /**
+     * IDEA 2:
+     * Dynamic Programming
+     *
+     * p.s. 剑指源码
+     *
+     */
+     public int uniquePaths( int m, int n ) {
+         int[][] dp = new int[m][n];
+         return helper( m - 1, n - 1, dp );
+     }
+
+     private int helper( int i, int j, int[][] dp ) {
+         if( dp[i][j] == 0 ) {
+             if( i == 0 || j == 0 )
+                 dp[i][j] = 1;
+             else
+                 dp[i][j] = helper( i - 1, j, dp) + helper( i, j - 1, dp);
+         }
+
+         return dp[i][j];
+     }
+
+    /**
+     * IDEA 3:
+     * Dynamic Programming
+     *
+     * p.s. 剑指源码
+     *
+     */
+
+     public int uniquePaths( int m, int n ) {
+         int[] dp = new int[n];
+         Arrays.fill(dp, 1);
+         for( int i = 1; i < m; ++i ) {
+             for( int j = 1; j < n; ++j ) {
+                 dp[j] += dp[j - 1];
+             }
+         }
+         return dp[n-1];
+     }
+
     // Question 67
     /**
      * IDEA 1:
@@ -35,7 +104,7 @@ class Solution {
      }
 
     /**
-     * IDEA 2:
+      * IDEA 2:
      * 剑指源码
      */
      public String addBinary( String a, String b ) {
