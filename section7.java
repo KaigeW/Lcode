87b01a42c2f338e8eb650908799f153fc6516075
Kaige <my476759488@hotmail.com>
2021-12-29 16:59:03 -0600

    one medium added (20 more to go!)

--- section7.java
+++ section7.java
@@ -69,6 +69,87 @@ class Solution {
          return dp[n-1];
      }
 
+
+    // Question 64
+    /**
+     * IDEA 1:
+     * Dynamic Programming
+     *
+     * Similar to 剑指 idea 1
+     *
+     */
+     public int minPathSum( int[][] grid ) {
+         int[][] dp = new int [grid.length][grid[0].length];
+
+         // initialize the fixed mined length
+         dp[0][0] = grid[0][0];
+         for( int i = 1; i < grid.length; ++i )
+             dp[i][0] = dp[i - 1][0] + grid[i][0];
+         for( int i = 1; i < grid[0].length; ++i )
+             dp[0][i] = dp[0][i - 1] + grid[0][i];
+
+         for( int i = 1; i < grid.length; ++i ) {
+             // line 86 87 can be replaced by the below command 剑指源码
+             // dp[i][0] = dp[i - 1][0] + grid[i][0];
+             for( int j = 1; j < grid[0].length; ++j ) {
+                 dp[i][j] = grid[i][j] +
+                   (dp[i - 1][j] > dp[i][j - 1] ? dp[i][j - 1] : dp[i - 1][j]);
+             }
+         }
+
+         return dp[grid.length - 1][grid[0].length - 1];
+     }
+
+
+
+    /**
+     * IDEA 2:
+     * Dynamic Programming
+     *
+     * Use 1D array(smaller memory) use at all
+     *
+     * same as 剑指 idea2
+     *
+     */
+     public int minPathSum( int[][] grid ) {
+         int[] dp = new int[grid[0].length];
+         dp[0] = grid[0][0];
+         for( int i = 1; i < grid[0].length; ++i )
+             dp[i] = dp[i - 1] + grid[0][i];
+
+         for( int i = 1; i < grid.length; ++i ) {
+             dp[0] = dp[0] + grid[i][0];
+             for( int j = 1; j < grid[0].length; ++j ) {
+                 dp[j] = grid[i][j] +
+                   ( dp[j] > dp[j - 1]? dp[j - 1] : dp[j] );
+             }
+         }
+         return dp[grid[0].length - 1];
+     }
+
+
+    /**
+     * IDEA 3:
+     * Dynamic Programming
+     *
+     * No extra memory use at all
+     *
+     */
+     public int minPathSum( int[][] grid ) {
+         for( int i = 1; i < grid[0].length; ++i )
+             grid[0][i] = grid[0][i - 1] + grid[0][i];
+
+         for( int i = 1; i < grid.length; ++i ) {
+             grid[i][0] = grid[i - 1][0] + grid[i][0];
+             for( int j = 1; j < grid[0].length; ++j ) {
+                 grid[i][j] = grid[i][j] +
+                   (grid[i - 1][j] > grid[i][j - 1] ? grid[i][j - 1] : grid[i - 1][j]);
+             }
+         }
+
+         return grid[grid.length - 1][grid[0].length - 1];
+     }
+
     // Question 67
     /**
      * IDEA 1:
